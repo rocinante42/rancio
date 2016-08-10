a9ea154f289585b3aeb56785e716f93f0dd804bf
rocinante42 <rovertoo@gmail.com>
2016-08-10 13:07:27 -0600

    parsing json

--- app.js
+++ app.js
@@ -152,12 +152,12 @@ app.get('/mdb', function(req, res){
     //res.send(req.query.movie);
   });
 });
-/*app.get('/mdbtest', function(req, res){
+app.get('/mdbtest', function(req, res){
   mdb.searchMovie({query: req.query.movie }, function(err, resp){
-    res.send(resp['results']['0']['id']);
+    res.send(resp.body.results.0.id);
     //res.send(req.query.movie);
   });
-});*/
+});
 
 
 //Setting the listen method to our enviroment port
