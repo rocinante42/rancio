ae7a0673eae51a9537bcc5e08ecc21d61d1ca018
rocinante42 <rovertoo@gmail.com>
2016-08-10 16:50:49 -0600

    last

--- app.js
+++ app.js
@@ -64,7 +64,7 @@ function receivedMessage(event) {
         // keywords and send back the corresponding example. Otherwise, just echo
         // the text we received.
         switch (messageText) {
-            case 'poster':
+            case 'star':
                 sendPosterMessage(senderID, messageText);
                 break;
 
