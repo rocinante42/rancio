96ddddda764c1a95644c9e72d376f269251b23a7
rocinante42 <rovertoo@gmail.com>
2016-08-10 11:18:19 -0600

    webhook

--- app.js
+++ app.js
@@ -5,9 +5,23 @@ var app = express();
 //process.env.PORT lets the port be set by Heroku
 var port = process.env.PORT || 8080;
 //routes
+//webhook
+app.get('/webhook', function(req, res) {
+  if (req.query['hub.mode'] === 'subscribe' &&
+      req.query['hub.verify_token'] === 'rancio_bot') {
+    console.log("Validating webhook");
+    res.status(200).send(req.query['hub.challenge']);
+  } else {
+    console.error("Failed validation. Make sure the validation tokens match.");
+    res.sendStatus(403);
+  }
+});
+//
 app.get('/', function(req, res){
   res.send("Hello world");
 });
+
+
 //Setting the listen method to our enviroment port
 app.listen(port, function(){
   console.log("Node JS server launched, running on http:localhost//:"+ port);
